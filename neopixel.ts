ef02d55db7db1273777a8ccf761f1c9ff020376a
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-09-05 03:19:38 +0900

    Updates.

--- neopixel.ts
+++ neopixel.ts
@@ -25,23 +25,23 @@ enum NeoPixelColors {
 }
 
 enum NeoPixelPins {
-    //% block P0
+    //% block=P0
     P0 = DigitalPin.P0,
-    //% block P1
+    //% block=P1
     P1 = DigitalPin.P1,
-    //% block P2
+    //% block=P2
     P2 = DigitalPin.P2,
-    //% block P8
+    //% block=P8
     P8 = DigitalPin.P8,
-    //% block P12
+    //% block=P12
     P12 = DigitalPin.P12,
-    //% block P13
+    //% block=P13
     P13 = DigitalPin.P13,
-    //% block P14
+    //% block=P14
     P14 = DigitalPin.P14,
-    //% block P15
+    //% block=P15
     P15 = DigitalPin.P15,
-    //% block P16
+    //% block=P16
     P16 = DigitalPin.P16
 };
 /**
